08addbdf305266471cfe87306169f53af5b83106
Amin <a.normohammadi83@gmail.com>
2024-09-22 23:43:54 +0330

    add files and folders

---
+++ app.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const dotEnv = require("dotenv");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+
+//* Load Config
+dotEnv.config({ path: "./config/config.env" });
+
+//* Database connection
+connectDB();
+
+const app = express();
+
+//* Logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+//* View Engine
+app.use(expressLayout);
+app.set("view engine", "ejs");
+app.set("layout", "./layouts/mainLayout");
+app.set("views", "views");
+
+//* BodyPaser
+app.use(express.urlencoded({ extended: false }));
+
+//* Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//* Routes
+app.use("/", require("./routes/blog"));
+app.use("/users", require("./routes/users"));
+app.use("/dashboard", require("./routes/dashboard"));
+
+//* 404 Page
+app.use((req, res) => {
+    res.render("404", { pageTitle: "صفحه پیدا نشد | 404", path: "/404" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const userController = require("../controllers/userController")
+
+const router = new Router();
+
+//  @desc   Login Page
+//  @route  GET /users/login
+router.get("/login", userController.login ) 
+
+//  @desc   Register Page
+//  @route  GET /users/register
+router.get("/register", userController.register ) 
+
+//  @desc   Register Handle
+//  @route  POST /users/register
+router.post("/register", userController.createUser)
+
+module.exports = router;
