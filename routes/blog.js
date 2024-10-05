0d0e6d99715ec6177ba4681eeecb8b28ca5618e1
unknown <a.normohammadi83@gmail.com>
2024-10-06 00:13:59 +0330

    add captcha

--- routes/blog.js
+++ routes/blog.js
@@ -12,4 +12,16 @@ router.get("/", blogController.getIndex);
 //  @route  GET /post/:id
 router.get("/post/:id", blogController.getSinglePost);
 
+//  @desc   Weblog contace Page
+//  @route  GET /Contact
+router.get("/contact", blogController.getContactPage);
+
+//  @desc   Weblog munric captcha
+//  @route  GET /captcha.png
+router.get("/captcha.png", blogController.getCaptcha);
+
+//  @desc   handle contact Page
+//  @route  GET /Contact
+router.post("/contact", blogController.handleContactPage);
+
 module.exports = router;
